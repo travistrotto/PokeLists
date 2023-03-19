a1e569867e80b0f5529f5910a19f4c083472cd30
travistrotto <travis.trotto@gmail.com>
2023-03-19 14:32:57 -0400

    Filename Method

--- selector.js
+++ selector.js
@@ -1,3 +1,4 @@
+
 // Find the divs under each p with a generation's name
 const gen1Div = document.querySelector('#gen1');
 const gen2Div = document.querySelector('#gen2');
@@ -10,46 +11,44 @@ const listSelectedDiv = document.querySelector('.ListSelected');
 // Keep track of the selected images
 const selectedImages = [];
 
-// Load images from each folder separately
-const folders = ['gen1', 'gen2', 'gen3'];
-folders.forEach(folder => {
-    // Load images from the folder
-    images.forEach(image => {
-        // Create a new img element
-        const img = document.createElement('img');
-        img.src = `img/regular/${folder}/${image}`;
-        img.alt = image.split('.')[0];
-        img.classList.add('sprite');
+// Add each image to the appropriate generation's div
+images.forEach(image => {
+    // Create a new img element
+    const img = document.createElement('img');
+    img.src = `img/regular/${image}`;
+    img.alt = image.split('.')[0];
+    img.classList.add('sprite');
 
-        // Add an event listener to log the image name when it is clicked
-        img.addEventListener('click', () => {
-            // Remove the "selected" class from any previously selected image
-            const selectedImg = document.querySelector('.selected');
-            if (selectedImg) {
-                selectedImg.classList.remove('selected');
-            }
-            // Add the "selected" class to the clicked image
-            img.classList.add('selected');
-            console.log(img.alt);
-
-            // Add the selected image to the selectedImages array
-            selectedImages.push(img);
-
-            // Create a new img element for the selected image and append it to the ListSelected div
-            const selectedImgElement = document.createElement('img');
-            selectedImgElement.src = img.src;
-            selectedImgElement.alt = img.alt;
-            listSelectedDiv.appendChild(selectedImgElement);
-        });
-
-        // Add the img element to the appropriate div based on the folder name
-        if (folder === 'gen1') {
-            gen1Div.appendChild(img);
-        } else if (folder === 'gen2') {
-            gen2Div.appendChild(img);
-        } else if (folder === 'gen3') {
-            gen3Div.appendChild(img);
+    // Add an event listener to log the image name when it is clicked
+    img.addEventListener('click', () => {
+        // Remove the "selected" class from any previously selected image
+        const selectedImg = document.querySelector('.selected');
+        if (selectedImg) {
+            selectedImg.classList.remove('selected');
         }
-        // + Add more conditions for other generations here
+        // Add the "selected" class to the clicked image
+        img.classList.add('selected');
+        console.log(img.alt);
+
+        // Add the selected image to the selectedImages array
+        selectedImages.push(img);
+
+        // Create a new img element for the selected image and append it to the ListSelected div
+        const selectedImgElement = document.createElement('img');
+        selectedImgElement.src = img.src;
+        selectedImgElement.alt = img.alt;
+        listSelectedDiv.appendChild(selectedImgElement);
     });
+
+    // Add the img element to the appropriate div
+    // Add the img element to the appropriate div
+    if (image.startsWith('1')) {
+        gen1Div.appendChild(img);
+    } else if (image.startsWith('2')) {
+        gen2Div.appendChild(img);
+    } else if (image.startsWith('3')) {
+        gen3Div.appendChild(img);
+    }
+    // + Add more conditions for other generations here
 });
+
